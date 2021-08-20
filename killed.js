dea69efe6226449c1a224ad1ff352f2562aa7eef
721HEMN <89218186+721HEMN@users.noreply.github.com>
2021-08-20 06:41:47 +0300

    Update killed.js

--- killed.js
+++ killed.js
@@ -92,7 +92,7 @@ client.on("message", msg => {
 });
 /////Reklam////
 client.on('ready' , () => {
-var join = client.channels.get("id voice")/// ئاید ڤۆیس
+var join = client.channels.get("877603234263482388")/// ئاید ڤۆیس
 if (join) join.join()
 })
 
@@ -102,13 +102,13 @@ if(message.channel.type === 'dm') {
  
 if (message.content.startsWith("https://discord.gg/")) {
  
-  message.author.send(`> link server`)/// لینکە سیرڤەرە خۆت
+  message.author.send(`> https://discord.gg/6UVtqFzPgB`)/// لینکە سیرڤەرە خۆت
  
-  client.channels.get("id text").send(
+  client.channels.get("877603220753612811").send(
 `> Nerdra la layan <@${message.author.id}> 
 ${message.content}`)
 }
     }
         })
 
-client.login("");
+client.login("ODc4MDA4MzA0MDA0MTI4Nzk4.YR669A.BwzjhzO1DPzkRRw-UbVXUHiCWK0");
